82df93276a9fb7493521c6352d0f56e5deb9c0b2
ahammed hinas <82311707+hinasahammed@users.noreply.github.com>
2024-08-11 18:16:14 +0530

    some changes

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -15,55 +15,57 @@ class HomeView extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              FutureBuilder(
-                future: HomeServices().fetchTask(),
-                builder: (context, snapshot) {
-                  if (snapshot.data == null ||
-                      snapshot.data!.isEmpty ||
-                      !snapshot.hasData) {
-                    return const Text("No task found");
-                  } else {
-                    return ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) => Card(
-                        child: ListTile(
-                          title: Text(
-                            snapshot.data![index],
-                            style: theme.textTheme.bodyLarge!.copyWith(
-                              color: theme.colorScheme.primary,
-                            ),
-                          ),
-                          subtitle: FutureBuilder(
-                            future: HomeServices().fetchDate(),
-                            builder: (context, snapshot) {
-                              if (snapshot.data == null) {
-                                return Text(
-                                    DateFormat.yMMMd().format(DateTime.now()));
-                              } else {
-                                return Text(
-                                  snapshot.data![index],
-                                  style: theme.textTheme.labelLarge!.copyWith(
-                                    color: theme.colorScheme.primary
-                                        .withOpacity(.4),
-                                  ),
-                                );
-                              }
-                            },
-                          ),
-                          trailing: Icon(
-                            Icons.delete,
-                            color: theme.colorScheme.error,
-                          ),
+          child: FutureBuilder(
+            future: HomeServices().fetchTask(),
+            builder: (context, snapshot) {
+              if (snapshot.data == null ||
+                  snapshot.data!.isEmpty ||
+                  !snapshot.hasData) {
+                return Center(
+                    child: Text(
+                  "No task found",
+                  style: theme.textTheme.titleLarge!.copyWith(
+                    color: theme.colorScheme.onSurface,
+                  ),
+                ));
+              } else {
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) => Card(
+                    child: ListTile(
+                      title: Text(
+                        snapshot.data![index],
+                        style: theme.textTheme.bodyLarge!.copyWith(
+                          color: theme.colorScheme.primary,
                         ),
                       ),
-                    );
-                  }
-                },
-              ),
-            ],
+                      subtitle: FutureBuilder(
+                        future: HomeServices().fetchDate(),
+                        builder: (context, snapshot) {
+                          if (snapshot.data == null) {
+                            return Text(
+                                DateFormat.yMMMd().format(DateTime.now()));
+                          } else {
+                            return Text(
+                              snapshot.data![index],
+                              style: theme.textTheme.labelLarge!.copyWith(
+                                color:
+                                    theme.colorScheme.primary.withOpacity(.4),
+                              ),
+                            );
+                          }
+                        },
+                      ),
+                      trailing: Icon(
+                        Icons.delete,
+                        color: theme.colorScheme.error,
+                      ),
+                    ),
+                  ),
+                );
+              }
+            },
           ),
         ),
       ),
